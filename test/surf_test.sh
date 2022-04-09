b532cda5cf1644ae5700a00651c66f3d0ad492a2
Dylan <royd4@wwu.edu>
2022-04-08 17:49:04 -0700

    dylan: streamlined surf testing execution

--- test/surf_test.sh
+++ test/surf_test.sh
@@ -5,3 +5,18 @@
 # * This software is under the terms of the BSD license.  * See README for more details.
 #
 # Test script for the Surf API
+
+while getopts d flag
+do
+  case "${flag}" in
+    d) debug="debug";;
+  esac
+done
+
+cd ../src/surf
+make
+cd ../../test
+make $debug
+
+./surf
+make clean
