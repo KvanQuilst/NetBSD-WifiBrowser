1a43421888416accf4ec868cbc0c360f2b7732d0
Dylan <royd4@wwu.edu>
2022-04-27 21:36:51 -0700

    dylan: gui

--- src/hangten/hangten.c
+++ src/hangten/hangten.c
@@ -41,22 +41,19 @@ int main(int argc, char **argv)
 
 int main()
 {
-  // Open a display.
   Display *d = XOpenDisplay(0);
 
   if (d)
     {
-      // Create the window
-      Window w = XCreateWindow(d, DefaultRootWindow(d), 0, 0, 200,
-			       100, 0, CopyFromParent, CopyFromParent,
-			       CopyFromParent, 0, 0);
+      int scr = DefaultScreen(d);
+      Window w = XCreateSimpleWindow(d, DefaultRootWindow(d), 0, 0, 200,
+			       100, 0, BlackPixel(d,scr), WhitePixel(d, scr));
 
-      // Show the window
       XMapWindow(d, w);
       XFlush(d);
 
-      // Sleep long enough to see the window.
       sleep(10);
     }
+
   return 0;
 }
