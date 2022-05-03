80637507e390eff2375ffd2f4d72ba2a3288ad09
Dylan <royd4@wwu.edu>
2022-05-02 18:35:08 -0700

    dylan: minor changes

--- src/surf/surf.c
+++ src/surf/surf.c
@@ -1,6 +1,6 @@
 /*
  * Surf API / surf.c
- * Copyright (c) 2022 Dylan Eskew, Stephen Loudiana, Kevin McGrane
+ * Copyright (c) 2022 Dylan Eskew
  * * This software is under the terms of the BSD license.  * See README for more details.
  *
  * Central point for processing requests made against the surf API
